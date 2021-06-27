49920048eedfa63e75f8b601c97d4cfea7061586
juniorfreitas <josedejesusjunior@gmail.com>
2021-06-27 00:07:19 -0300

    Iniciando projeto de upload para o s3 via nodejs

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const s3FolderUpload = require("s3-folder-upload");
+const { readdirSync, unlinkSync } = require("fs");
+
+const folderBkp = process.env.folderBkp
+  ? process.env.folderBkp
+  : __dirname + "/backups";
+
+const credentials = {
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: process.env.region,
+  bucket: process.env.bucket,
+};
+
+const options = {
+  useFoldersForFileTypes: false,
+  useIAMRoleCredentials: false,
+};
+
+const bkp = readdirSync(folderBkp);
+
+if (bkp.length >= 1) {
+  const upload = new Promise((resolve, reject) => {
+    resolve(s3FolderUpload(folderBkp, credentials, options));
+  });
+
+  upload
+    .then((res) => {})
+    .then((res) => {
+      bkp.forEach((file) => {
+        unlinkSync(`${folderBkp}/${file}`);
+        console.log(`Arquivo removido ${file}`);
+      });
+    });
+} else {
+  console.log("nenhum arquivo para upload");
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Upload arquivos em pasta via nodejs
+
+Para fazer as configurações utilize o seguinte comando
+
+* yarn install 
+  ou
+* npm install
+
+* cp .env-example .env
+
+* yarn start
+ou
+* npm run start
